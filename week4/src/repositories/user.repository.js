a5de20accc6fa666ed766b7aabf11232cd3a7e39
Vanshika Khandelwal <vanshikak.hestabit@gmail.com>
2025-11-26 17:36:01 +0530

    repository pattern

---
+++ week4/src/repositories/user.repository.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+
+class UserRepository {
+  // Create new user
+  static async create(userData) {
+    const user = new User(userData);
+    return user.save();
+  }
+
+  // Find user by ID
+  static async findById(id) {
+    return User.findById(id);
+  }
+
+  // Paginated users
+  static async findPaginated(page = 1, limit = 10) {
+    const skip = (page - 1) * limit;
+    const users = await User.find().skip(skip).limit(limit);
+    return users;
+  }
+
+  // Update user
+  static async update(id, updateData) {
+    return User.findByIdAndUpdate(id, updateData, { new: true });
+  }
+
+  // Delete user
+  static async delete(id) {
+    return User.findByIdAndDelete(id);
+  }
+}
+
+module.exports = UserRepository;
