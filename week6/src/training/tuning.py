042042effa89ccca1bdcf7af1175d9a0b5cf8e10
Vanshika Khandelwal <vanshikak.hestabit@gmail.com>
2025-12-22 18:37:05 +0530

    refractor: code standard

--- week6/src/training/tuning.py
+++ week6/src/training/tuning.py
@@ -1,28 +1,26 @@
-# training/tuning.py
-
 import pandas as pd
 import json
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 
-# ---------------- LOAD DATA ----------------
+# LOAD DATA 
 X_train = pd.read_pickle("src/data/processed/X_train.pkl")
 y_train = pd.read_pickle("src/data/processed/y_train.pkl")
 
-# ---------------- MODEL ----------------
+#  MODEL
 log_reg = LogisticRegression(max_iter=1000)
 
-# ---------------- PARAMETER GRID ----------------
-param_grid = {
+# PARAMETER GRID 
+my_settings = {
     "C": [0.01, 0.1, 1, 10],
     "penalty": ["l2"],
     "solver": ["lbfgs"]
 }
 
-# ---------------- GRID SEARCH ----------------
+#  GRID SEARCH (training+testing+comparing)
 grid_search = GridSearchCV(
     estimator=log_reg,
-    param_grid=param_grid,
+    param_grid=my_settings,
     scoring="roc_auc",
     cv=5,
     n_jobs=-1
@@ -30,7 +28,7 @@ grid_search = GridSearchCV(
 
 grid_search.fit(X_train, y_train)
 
-# ---------------- RESULTS ----------------
+# RESULTS 
 best_params = grid_search.best_params_
 best_score = grid_search.best_score_
 
@@ -40,7 +38,7 @@ results = {
     "best_roc_auc": best_score
 }
 
-# ---------------- SAVE RESULTS ----------------
+# SAVE RESULTS in JSON
 with open("src/tuning/results.json", "w") as f:
     json.dump(results, f, indent=4)
 
