e503ad5f62fd1e356e6062ec4ef2a20f8f16e77f
Vanshika Khandelwal <vanshikak.hestabit@gmail.com>
2026-01-06 12:00:14 +0530

    generated dense and sparse embedding

--- week7-genai/src/embeddings/embedder.py
+++ week7-genai/src/embeddings/embedder.py
@@ -1,21 +1,36 @@
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_qdrant import QdrantVectorStore
 from src.pipelines.ingest import chunk_load
+from langchain_qdrant import FastEmbedSparse, RetrievalMode
+from src.vectorstore.qdrant_client import get_QD_client
+
 
 def embedding_model():
   return HuggingFaceBgeEmbeddings(
-    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    model_name="BAAI/bge-base-en-v1.5"
 
 )
 embedding = embedding_model()
 
+client = get_QD_client()
+def sparseModel():
+   return FastEmbedSparse(model_name="Qdrant/bm25")
+
+get_sparse_embedding=sparseModel()
 chunks = chunk_load()
 
-vector_store = QdrantVectorStore.from_documents(
-    documents=chunks,
-    embedding=embedding,
-    url="http://localhost:6333",
-    collection_name="day1-RAG"
+vector_store = QdrantVectorStore(
+  client=client,
+  collection_name="genai-learning",
+  sparse_embedding=get_sparse_embedding,
+  embedding=embedding,
+  vector_name="dense",
+  sparse_vector_name="sparse",
+  retrieval_mode=RetrievalMode.HYBRID,
+
+    
 )
+
+vector_store.add_documents(documents=chunks)
 
 print("Embedding stored in Qdrant")
