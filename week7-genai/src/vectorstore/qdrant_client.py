0cef64e8533e92149f461d712d64b44ccf872d7f
Vanshika Khandelwal <vanshikak.hestabit@gmail.com>
2026-01-13 16:16:48 +0530

    added image collection

--- week7-genai/src/vectorstore/qdrant_client.py
+++ week7-genai/src/vectorstore/qdrant_client.py
@@ -16,10 +16,39 @@ def get_QD_client():
             collection_name=COLLECTION_NAME,
             vectors_config={
                 "dense":VectorParams(
-                    size=768,
+                    size=512,
                     distance=Distance.COSINE
                 ),
                 "image_dense": VectorParams(size=512, distance=Distance.COSINE)
+            },
+            sparse_vectors_config={
+                "sparse":SparseVectorParams()
+            }
+            
+        )
+        print("hybrid collection created!!")
+
+    return client
+
+
+def get_QD_client_for_pdf_rag():
+    COLLECTION_NAME ="genai-hestabit"
+
+    client = QdrantClient(
+        host ="localhost",
+        port = 6333
+    )
+     
+    if not client.collection_exists(COLLECTION_NAME):
+        client.delete_collection(COLLECTION_NAME)
+        print("Old collection deleted")
+        client.create_collection(
+            collection_name=COLLECTION_NAME,
+            vectors_config={
+                "dense":VectorParams(
+                    size=768,
+                    distance=Distance.COSINE
+                ),
             },
             sparse_vectors_config={
                 "sparse":SparseVectorParams()
