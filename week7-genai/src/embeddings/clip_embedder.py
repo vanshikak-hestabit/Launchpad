904ae142593a645f6c2906696ecbba90565c3153
Vanshika Khandelwal <vanshikak.hestabit@gmail.com>
2026-01-13 16:27:56 +0530

    image embedding

--- week7-genai/src/embeddings/clip_embedder.py
+++ week7-genai/src/embeddings/clip_embedder.py
@@ -1,38 +1,42 @@
 from PIL import Image
 import torch
 from transformers import CLIPProcessor, CLIPModel
-from src.vectorstore.qdrant_client import get_QD_client 
+from src.vectorstore.qdrant_client import get_QD_client
 from qdrant_client.models import PointStruct
 from src.pipelines.image_ingest import load_img, OCR, gen_cap
 
 client = get_QD_client()
 COLLECTION_NAME = "genai-learning"
 
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")          # generates embeddings
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")  # prepares img + txt for clip
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 
 def generate_embeddings(image_path, caption):
-    image = Image.open(image_path).convert('RGB')
-    inputs = processor(text=[caption], images=image, return_tensors="pt", padding=True)
-    with torch.no_grad():       # not training just calculating
+    image = Image.open(image_path).convert("RGB")
+    inputs = processor(text=[caption], images=image, return_tensors="pt", padding=True).to(device)
+    
+    with torch.no_grad():
         image_embeds = model.get_image_features(inputs['pixel_values'])
         text_embeds = model.get_text_features(inputs['input_ids'])
+    
+    return image_embeds[0].cpu().numpy(), text_embeds[0].cpu().numpy()
 
-    return image_embeds[0].numpy(), text_embeds[0].numpy()
 
 def embed_storeAll(folder):
     images = load_img(folder)
     ocr_texts = [OCR(p) for p in images]
     captions = [gen_cap(p) for p in images]
+
     for idx, img in enumerate(images, start=1):
         img_vec, txt_vec = generate_embeddings(img, captions[idx-1])
+        
         point = PointStruct(
-            id = idx,
+            id=idx,  # Consider using UUID for safety
             vector={
-                "image_dense":img_vec,
-                "text_dense": txt_vec.tolist()
-                
+                "image_dense": img_vec.tolist(),
+                "dense": txt_vec.tolist()
             },
             payload={
                 "image_path": img,
@@ -42,8 +46,9 @@ def embed_storeAll(folder):
             }
         )
         client.upsert(collection_name=COLLECTION_NAME, points=[point])
- 
+    
     print(f"All {len(images)} embeddings stored in Qdrant!")
+
 
 if __name__ == "__main__":
     embed_storeAll("src/data/raw")
