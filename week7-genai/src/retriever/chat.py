6c3959d98adfa51188af340498ae98001dd6c79e
Vanshika Khandelwal <vanshikak.hestabit@gmail.com>
2026-01-13 16:28:56 +0530

    img-txt LLM

--- week7-genai/src/retriever/chat.py
+++ week7-genai/src/retriever/chat.py
@@ -12,24 +12,9 @@ client = OpenAI(
     api_key=os.environ.get("OPEN_API_KEY")   
 )
 
+## -----text to image-------
 # results = text_to_image(user_query)
-results = image_to_image("src/data/raw/gi8.png")
-# caption, ocr_text, image_path = image_to_text("src/data/raw/gi5.png")
 
-
-### -----image to image-------
-SYSTEM_PROMPT = f"""
-You are a helpful AI assistant who retrives the similar images strictly based on
-the provided context retrieved from images.
-
-Rules:
-- Use ONLY the given context
-- Mention the relevant page number(s)
-- If the answer is not in the context, say you don’t know
-"""
-
-
-### -----text to image-------
 # SYSTEM_PROMPT = f"""
 # You are a helpful AI assistant who retrieves the image strictly based on
 # the provided context retrieved from images.
@@ -40,23 +25,45 @@ Rules:
 # - If the answer is not in the context, say you don’t know
 # """
 
+# response = client.chat.completions.create(
+#     model="llama-3.3-70b-versatile",
+#     messages=[
+#         {"role": "system", "content": SYSTEM_PROMPT},
+#         {"role": "user", "content": user_query},
+#     ]
+# )
+
+# for item in results:
+#     img = Image.open(item.payload["image_path"])
+#     img.show()
+
+
+## ------for image->img -----
+# results = image_to_image("src/data/raw/gi9.png")
+
+# for item in results:
+#     img = Image.open(item.payload["image_path"])
+#     img.show()
+
 
 #### -----image to text-------
-# SYSTEM_PROMPT = f"""
-# You are a helpful AI assistant who explains Explain the image
-#  in simple words using ONLY the information below.
 
-# Caption:
-# {caption}
+caption, ocr_text, image_path = image_to_text("src/data/raw/gi2.png")
+SYSTEM_PROMPT = f"""
+You are a helpful AI assistant who explains Explain the image
+ in simple words using ONLY the information below.
 
-# Text found in image:
-# {ocr_text}
+Caption:
+{caption}
 
-# Rules:
-# - Simple explanation
-# - 2–4 lines
-# - No outside knowledge
-# """
+Text found in image:
+{ocr_text}
+
+Rules:
+- Simple explanation
+- 2-4 lines
+- No outside knowledge
+"""
 
 response = client.chat.completions.create(
     model="llama-3.3-70b-versatile",
@@ -66,12 +73,6 @@ response = client.chat.completions.create(
     ]
 )
 
-### for image->img and txt->img
-print(f"{response.choices[0].message.content}")
-for item in results:
-    img = Image.open(item.payload["image_path"])
-    img.show()
-   
+print(response.choices[0].message.content)
 
-### for img -> txt
-# print(response.choices[0].message.content)
+
