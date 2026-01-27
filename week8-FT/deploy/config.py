8fdace7646884c51e5b6c49e258e1a06e16f150e
Vanshika Khandelwal <vanshikak.hestabit@gmail.com>
2026-01-27 15:21:37 +0530

    configuration file

---
+++ week8-FT/deploy/config.py
@@ -0,0 +1,8 @@
+
+MODEL_PATH = "/home/vanshikakhandelwal/Launchpad/week8-FT/models/model-q4_0.gguf"
+N_CTX = 2048
+N_THREADS = 8
+N_GPU_LAYERS = 0  
+DEFAULT_TEMP = 0.7
+DEFAULT_TOP_P = 0.9
+DEFAULT_TOP_K = 40
