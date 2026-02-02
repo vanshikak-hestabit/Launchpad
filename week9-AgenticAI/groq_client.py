08a6f8d847bcbdfd4875cf9c9da12db5a105563e
vanshikak-hestabit <vanshikak.hestabit@gmail.com>
2026-02-02 22:13:22 +0530

    groq client

---
+++ week9-AgenticAI/groq_client.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+# Load environment variables
+load_dotenv()
+
+def create_model_client():
+   
+    return OpenAIChatCompletionClient(
+        model="llama-3.3-70b-versatile",
+        base_url="https://api.groq.com/openai/v1",
+        api_key=os.getenv("GROQ_API_KEY"),
+        model_info={
+            "vision": True,
+            "function_calling": True,
+            "json_output": True,
+            "family": "llama-3.3",
+            "structured_output": True,
+        }
+    )
