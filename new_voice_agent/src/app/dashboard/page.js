96d2bbfc2bfca5ee484c08d1b802ca7c4ca20d05
vanshikak-hestabit <vanshikak.hestabit@gmail.com>
2026-02-28 19:41:59 +0530

    dashboard page

--- new_voice_agent/src/app/dashboard/page.js
+++ new_voice_agent/src/app/dashboard/page.js
@@ -12,16 +12,7 @@ export default function DashboardPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [voiceOpen, setVoiceOpen] = useState(false);
-  const [calls, setCalls] = useState([]);
-
-  const totalCalls = calls.length;
-
-  const avgDuration =
-    calls.length > 0
-      ? Math.floor(
-          calls.reduce((sum, c) => sum + c.duration, 0) / calls.length
-        )
-      : 0;
+ 
   const router = useRouter();
 
   useEffect(() => {
@@ -34,15 +25,6 @@ export default function DashboardPage() {
 
       setName(user.user_metadata?.display_name || "User");
       setEmail(user.email || "");
-
-      const { data, error } = await supabase
-        .from("calls")
-        .select("*")
-        .eq("user_id", user.id);
-
-      if (!error && data) {
-        setCalls(data);
-      }
     };
 
     getUser();
@@ -87,10 +69,7 @@ export default function DashboardPage() {
         </div>
       </div>
 
-        <DashboardCards
-          totalCalls={totalCalls}
-          avgDuration={avgDuration}
-        />
+        <DashboardCards  />
 
       <div className="mt-8">
         <DashboardActivity />
