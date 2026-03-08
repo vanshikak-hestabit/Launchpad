dcfd09f96d89588db5cf4cbcab051115ff321ed1
vanshikak-hestabit <vanshikak.hestabit@gmail.com>
2026-03-08 21:41:11 +0530

    added analytics page

--- ai_companion/src/app/dashboard/analytics/page.js
+++ ai_companion/src/app/dashboard/analytics/page.js
@@ -1,3 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase/client";
+import AnalyticsCards from "@/components/AnalyticsCards";
+import AnalyticsGraphs from "@/components/AnalyticsGraphs";
+
 export default function AnalyticsPage() {
-    return <div>Analytics page</div>
+
+  const [conversations, setConversations] = useState([]);
+
+  const fetchConversations = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) return;
+
+    const { data, error } = await supabase
+    .from("conversations")
+    .select(`
+        *,
+        companions (
+        name
+        )
+    `)
+    .eq("user_id", user.id);
+
+    if (!error && data) {
+      setConversations(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchConversations();
+  }, []);
+
+  return (
+    <div className="mx-auto max-w-7xl px-4 py-8">
+
+      <h1 className="text-3xl font-bold mb-8">
+        Analytics Overview
+      </h1>
+
+      <AnalyticsCards conversations={conversations} />
+      <AnalyticsGraphs conversations={conversations} />
+
+    </div>
+  );
 }
